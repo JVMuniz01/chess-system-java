8661ae3f59150af4fe889f0175df07ad3d8263e0
Muniz <j040vmr@gmail.com>
2025-06-18 05:39:32 -0300

    Switching player each turn

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -6,13 +6,25 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
-
 public class ChessMatch {
+    
+    private int turn;
+    private Color currentPlayer;
     private Board board;
     
     public ChessMatch(){
         board = new Board(8,8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+    
+    public int getTurn(){
+        return turn;
+    }
+    
+    public Color getCurrentPlayer(){
+        return currentPlayer;
     }
     public ChessPiece[][]getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRaws()][board.getColumns()];
@@ -36,6 +48,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturePiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece)capturePiece;
     }
     
@@ -53,12 +66,23 @@ public class ChessMatch {
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is no possible moves for chosen piece");
         }
+        if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("The choen piece is not yours");
+        }
     }
     private void validateTargetPosition(Position source, Position target){
         if(!board.piece(source).possibleMove(target)){
             throw new ChessException("The chose piece can't move to target position");
         }
         
+    }
+    
+    private void nextTurn(){
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE; /**Se o jogador atual == WHITE, agora quem joga são as pretas.
+                                                                                    * caso contrário, sera o WHITE
+                                                                                    * 
+                                                                                    */
     }
     
     private void placeNewPiece(char coluumn, int row, ChessPiece piece){
